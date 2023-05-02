597e1ee29b92738eb387669a996ab339d751f9c6
masudrahman-dev <masudrahman.dev@gmail>
2023-05-03 03:58:42 +0600

    make signle file for data

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const data = require('./data/data.json')
-app.get('/', (req, res) => {
-  res.send(data)
-})
+const express = require("express");
+const app = express();
+const port = 3000;
+const data = require("./data/data.json");
+
+app.get("/", (req, res) => {
+  res.send(data);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});

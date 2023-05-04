5129bac6cd765d53da66a51e2bb64e6a3549e5d4
masudrahman-dev <masudrahman.dev@gmail>
2023-05-04 12:37:42 +0600

    update slider image url

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const data = require("./data/data.json");
 app.get("/home", (req, res) => {
   res.send(data);
 });
+app.get("/home/:id", (req, res) => {
+  const recefiData = data[0].find((recefi) => recefi._id === req.params.id);
+  console.log(recefiData);
+  res.send(recefiData)
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
